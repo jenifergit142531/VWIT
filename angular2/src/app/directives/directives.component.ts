d4ecd00abacbf02f089fdedca44cb5d5f9ae764f
jenifery <jenifers@revature.com>
2024-02-26 23:10:31 -0500

    1

---
+++ angular2/src/app/directives/directives.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directives',
+  templateUrl: './directives.component.html',
+  styleUrl: './directives.component.css'
+})
+export class DirectivesComponent {
+
+  PropShop:any[]=[
+    {pid:'p100',pname:'avisa',price:25000,availability:'yes',Updateddate:'2/26/2024'},
+    {pid:'p200',pname:'ivory',price:15000,availability:'no',Updateddate:'2/25/2024'},
+    {pid:'p300',pname:'laguna',price:20000,availability:'yes',Updateddate:'2/24/2024'},
+    {pid:'p400',pname:'macarena',price:5000,availability:'no',Updateddate:'2/23/2024'}
+  ];
+
+}
