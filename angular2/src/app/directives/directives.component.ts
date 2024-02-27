c814ce923c3a8bf376c9bcdb04bf39c548a5e0fa
jenifery <jenifers@revature.com>
2024-02-26 23:49:56 -0500

    2

--- angular2/src/app/directives/directives.component.ts
+++ angular2/src/app/directives/directives.component.ts
@@ -14,4 +14,15 @@ export class DirectivesComponent {
     {pid:'p400',pname:'macarena',price:5000,availability:'no',Updateddate:'2/23/2024'}
   ];
 
+  refresh():void{
+    this.PropShop=[
+      {pid:'p100',pname:'avisa',price:25000,availability:'yes',Updateddate:'2/26/2024'},
+    {pid:'p200',pname:'ivory',price:15000,availability:'no',Updateddate:'2/25/2024'},
+    {pid:'p300',pname:'laguna',price:20000,availability:'yes',Updateddate:'2/24/2024'},
+    {pid:'p400',pname:'macarena',price:5000,availability:'no',Updateddate:'2/23/2024'},
+    {pid:'p500',pname:'aaa',price:20000,availability:'yes',Updateddate:'2/24/2024'},
+    {pid:'p600',pname:'bbb',price:5000,availability:'no',Updateddate:'2/23/2024'},
+    ]
+  }
+
 }
