6155d376353a6bb079d77d9cd858cec2b1245031
jenifery <jenifers@revature.com>
2024-03-01 01:26:34 -0500

    4

--- demos/angular5/src/app/app-routing.module.ts
+++ demos/angular5/src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { authGuard } from './auth.guard';
 
 const routes: Routes = [
   {
     path:'login',component:LoginComponent
   },
   {
-    path:'dashboard',component:DashboardComponent
+    path:'dashboard',component:DashboardComponent,canActivate:[authGuard]
   },
   {
     path:'',redirectTo:'/login',pathMatch:'full'
